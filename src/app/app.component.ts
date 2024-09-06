d1394aa64e5900d11fefed6bb5641ba12770b307
Malen28lopez <malopez@asap-consulting.net>
2024-09-06 11:50:12 -0300

    shangular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+import { RouterOutlet } from '@angular/router';
+import { BuscadorComponent } from '../app/components/buscador/buscador.component';
+import { TablaPageComponent } from '../app/components/tabla-page/tabla-page.component';
+// import { AñadirPageComponent } from '../app/components/añadir-page';
+import { SidebarComponent } from '../app/components/sidebar/sidebar.component';
+ 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, SidebarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'heroes';
+  title = 'app-heroes';
 }
+ 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BuscadorComponent } from './components/buscador/buscador.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { TablaPageComponent } from './components/tabla-page/tabla-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BuscadorComponent,
+    SidebarComponent,
+    TablaPageComponent
   ],
-  imports: [
+  imports: [SidebarComponent, 
     BrowserModule,
     AppRoutingModule
   ],
+  exports : [SidebarComponent],
   providers: [],
-  bootstrap: [AppComponent]
+  // bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
